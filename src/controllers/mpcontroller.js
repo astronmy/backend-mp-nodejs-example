a25e88c13b24fb790816ec7c4f48ce3f158a1749
Renzo Vinci <renzvinci@gmail.com>
2022-09-28 23:04:52 -0300

    cambios

--- src/controllers/mpcontroller.js
+++ src/controllers/mpcontroller.js
@@ -1,25 +1,32 @@
-const {mercadopago} = require('../utils/mercadopago.js')
+const { mercadopago } = require('../utils/mercadopago.js')
 
-const createPreference = async(req,res) =>{
-   const items = req.body.items.map(item => {
-    const{title, price:unit_price, quantity} = item
-    return {title, unit_price, quantity}
+const createPreference = async (req, res) => {
+  const items = req.body.items.map(item => {
+    const { title, price: unit_price, quantity, image: picture_url } = item
+    const description = title
+    return { title, unit_price, quantity, description, picture_url }
   })
   let preference = {
     items,
-    statement_descriptor: "Camada3DH",
+    back_urls: {
+      success: "https://www.google.com",
+      failure: "https://www.digitalocean.com/",
+    },
+    payment_methods: {
+      installments: 1
+    },
   };
   mercadopago.preferences
     .create(preference)
     .then(function (response) {
       res.json(
-        {global: response.body.id}
+        { global: response.body.id }
       )
     })
     .catch(function (error) {
       console.log(error);
     });
-} 
+}
 
 module.exports = {
   createPreference
